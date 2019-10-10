ef66f40e6eff2762c14c4ab1d7e26da972505194
Joris de Jong <jorisxdejong@gmail.com>
2019-10-11 01:15:26 +0200

    data is now stored in spreadSheet (prices have a ',' and must be a '.')

--- files/Main.js
+++ files/Main.js
@@ -34,10 +34,17 @@ function include(filename){
 };
 
 function formatButton(inputfieldBp, inputFieldBo) {
-    var split1 = formatText(inputfieldBp);
-    var split2 = formatText(inputFieldBo);
-    Logger.log(split1);
-    Logger.log(split2);
+    var products = formatText(inputfieldBp);
+    var overview = formatText(inputFieldBo);
+    
+    var productNames = products[0];
+    var productNumbers = products[1];
+    var productPrices = products[2];
+    for(var i = 2; i <= (productNames.length + 1); i++) {
+      setCell('A', i, productNames[i - 2]);
+      setCell('G', i, productNumbers[i - 2]);
+      setCell('B', i, productPrices[i - 2]);
+    }
 
     setupSideBar();
 } 
@@ -47,12 +54,26 @@ function setCell(row, inputIndex, output) {
     .setValue(output);
 }
 
+
+// euro: /(€\d+\.\d{1,2})/g
+//whitespaces: /\s+ /g
 function formatText(inputField) {
-  //var split = inputField.split(/  +/g); deze split bij meerdere whitespaces
   var split = inputField.split(/\b\n/);
+  var numbers = [];
+  var prices = [];
+
   for(var i = 0; i < split.length; i++) {
-    split[i] = split[i].replace(/\s\s+/g, ' ')
+    split[i] = split[i].replace(/\s\s+/g, ' ');
+
+    var newNumber = parseFloat(split[i]);
+    numbers.push(newNumber);
+
+    var newPrice = split[i].match(/(€ \d+\,\d{1,2})/g);
+    prices.push(newPrice);
+
+    split[i] = split[i].replace(newNumber, '');
+    split[i] = split[i].replace(newPrice, '');
   }
 
-  return split;
+  return [split, numbers, prices];
 }
